0bf326ee0a21a0c49ccc8a682247bb88f38f770e
Kamrul Ashik <mdkamrul.islam@northsouth.edu>
2020-11-17 20:26:09 +0600

    homework 1 is done

---
+++ Homework Submission/Homework 1/1.asm
@@ -0,0 +1,21 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+; add your code here
+
+
+ADD AX, 1 
+ADD AX, 2 
+ADD AX, 4
+ 
+
+MOV CX, AX
+
+ret
+
+
+
+
