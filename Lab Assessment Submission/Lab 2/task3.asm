685d6ef78429098f4ae4967843bfc4cac5b51dd3
Kamrul Ashik <mdkamrul.islam@northsouth.edu>
2020-11-18 09:56:22 +0600

    task 3 and 4 submission

---
+++ Lab Assessment Submission/Lab 2/task3.asm
@@ -0,0 +1,17 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+; Define an array of 5 numbers using DUP
+
+
+	
+A DB 5 DUP(9)               
+	
+ret
+
+
+
+
---
+++ Lab Assessment Submission/Lab 2/task4.asm
@@ -0,0 +1,17 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+; Define an array with occuring 1 and 2 value
+
+
+	
+A DB 5 DUP(1,2)               
+	
+ret
+
+
+
+
